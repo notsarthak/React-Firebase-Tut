c564e0a1583ed082a92e813421d83e923d9b7e32
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-11-20 00:25:11 +0530

    Created Modal

--- firegram/src/comps/ImageGrid.js
+++ firegram/src/comps/ImageGrid.js
@@ -1,14 +1,13 @@
 import React from "react";
 import useFirestore from "../hooks/useFirestore";
 
-const ImageGrid = () => {
+const ImageGrid = ({setSelectedImg}) => {
     const {docs} = useFirestore("images");
-    console.log(docs);
     return (
         <div className="img-grid">
             {docs && docs.map( doc => {
                 return (
-                    <div className="img-wrap" key={doc.id}>
+                    <div className="img-wrap" key={doc.id} onClick={()=>setSelectedImg(doc.url)}>
                         <img src={doc.url} alt="uploaded" />
                     </div>
                 )
